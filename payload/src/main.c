531290ee2f7a3cfa26f8621d57bcffad417bd3bd
balika011 <balika011@gmail.com>
2020-08-17 03:25:58 +0200

    reread the mbm from the flash and patch it

--- payload/src/main.c
+++ payload/src/main.c
@@ -1,20 +1,19 @@
 #include <stdint.h>
+#include <string.h>
+
+#define read_from_flash(a1, a2, a3, a4, a5) ((int (*)(int, int, char *, int, unsigned int)) (0x89312E76 + 1))(a1, a2, a3, a4, a5)
 
 int main()
 {
-	*(uint16_t *) 0x8932EAD4 = 0xA55A; // RESET
-
-	/*
-	*(uint8_t *) 0x8932F18F = 1;
-	*(uint8_t *) 0x89314426 = 1;
-	*(uint8_t *) 0x8931D628 = 1;
-	*(uint8_t *) 0x8931D662 = 1;
-	*/
+	read_from_flash(4, 7, (char *) 0x90000000, 0, 0x40000);
 	
+	memcpy((char *) 0x89310000, (char *) 0x90000000, 0x40000);
+	
+	// insecure patch
 	*(uint16_t *) 0x893200A2 = 0x2001;
 	*(uint16_t *) 0x893200A4 = 0x4770;
-	
-	*(uint32_t *) 0x893100A0 = 0xEA006E19;
+
+	// jump
 	((void (*)()) 0x89310000)();
 	
 	return 0;
